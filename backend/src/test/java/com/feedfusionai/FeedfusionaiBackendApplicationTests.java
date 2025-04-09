df47363680d73ac577c097204c931a3d36883353
Hisham Abraham <hisham.abraham@icloud.com>
2025-04-08 21:13:18 -0400

    Add missing files

---
+++ backend/src/test/java/com/feedfusionai/FeedfusionaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedfusionai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedfusionaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
